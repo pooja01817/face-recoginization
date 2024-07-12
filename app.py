b7f6acbb5fe3e5ab15ab29db0eb68a49a59275b8
MANDADAPU POOJA <156198848+pooja01817@users.noreply.github.com>
2024-07-12 12:27:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, url_for, render_template
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = r'C:\Users\manda\OneDrive\Documents\face_rec\images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html', success=False)
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    name = request.form.get('name', '').strip()
+    if not name or file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        file_extension = file.filename.rsplit('.', 1)[1].lower()
+        filename = f"{name}.{file_extension}"
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('upload.html', success=True)
+
+if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(debug=True)
